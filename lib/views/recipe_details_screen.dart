6b8fcf5f15d9ff7046730640170780aad96ea5bb
wlong0711 <weilong02@gmail.com>
2025-07-08 17:52:48 +0800

    refactor codes

--- lib/views/recipe_details_screen.dart
+++ lib/views/recipe_details_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 import '../viewmodels/recipe_viewmodel.dart';
-import '../models/recipe.dart';
 
 class RecipeDetailsScreen extends StatefulWidget {
   final int recipeId;
